7174d8a80c9a89f08715d97da0deb46036d1852c
tuchenbin <cbtu@xglory.com>
2018-08-01 13:33:29 +0800

    添加一行

--- src/page/obDepartment/steps.vue
+++ src/page/obDepartment/steps.vue
@@ -44,6 +44,7 @@
             <Step title="已完成" content="这里是该步骤的描述信息"></Step>
             <Step title="已完成" content="这里是该步骤的描述信息"></Step>
             <Step title="进行中" content="这里是该步骤的描述信息"></Step>
+            
             <Step title="待进行" content="这里是该步骤的描述信息"></Step>
         </Steps>
 
